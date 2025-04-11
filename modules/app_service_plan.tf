2b5cd0f2ba18a310212185fa766018bcabc6c725
kishanmv <venkatkishan117@gmail.com>
2025-04-11 17:47:50 +0530

    updates appservice plan in modules

--- modules/app_service_plan.tf
+++ modules/app_service_plan.tf
@@ -1,4 +1,4 @@
-resource "azurerm_app_service_plan" "var.appservice_plan_name" {
+resource "azurerm_app_service_plan" "var.appservice_plan_gitname" {
   name                = var.aspname
   location            = var.location
   resource_group_name = azurerm_resource_group.kishan.name
